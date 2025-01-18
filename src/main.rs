7baa832526391d6be7fbc223b2c7fba4aa0167c8
FFudo <philipphofbauer@aon.at>
2025-01-19 00:04:57 +0100

    Added wgpu struct and constructor

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,81 @@
-use glfw::{fail_on_errors, Action, Context, Key};
+use glfw::{fail_on_errors, Action, Context, Key, Window};
+use wgpu;
+
+struct State<'a> {
+    instance: wgpu::Instance,
+    surface: wgpu::Surface<'a>,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    size: (i32, i32),
+    window: &'a mut Window,
+}
+
+impl<'a> State<'a> {
+    async fn new(window: &'a mut Window) -> Self {
+        let size = window.get_size();
+        let instance_descriptor = wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::all(),
+            ..Default::default()
+        };
+        let instance = wgpu::Instance::new(&instance_descriptor);
+
+        let target = unsafe { wgpu::SurfaceTargetUnsafe::from_window(&window) }.unwrap();
+        let surface = unsafe { instance.create_surface_unsafe(target) }.unwrap();
+
+        let adapter_descriptor = wgpu::RequestAdapterOptionsBase {
+            power_preference: wgpu::PowerPreference::default(),
+            compatible_surface: Some(&surface),
+            force_fallback_adapter: false,
+        };
+
+        let adapter = instance.request_adapter(&adapter_descriptor).await.unwrap();
+
+        let device_descriptor = wgpu::DeviceDescriptor {
+            required_features: wgpu::Features::empty(),
+            required_limits: wgpu::Limits::default(),
+            label: Some("Device"),
+            memory_hints: wgpu::MemoryHints::Performance,
+        };
+
+        let (device, queue) = adapter
+            .request_device(&device_descriptor, None)
+            .await
+            .unwrap();
+
+        let surface_capabilities = surface.get_capabilities(&adapter);
+        let surface_format = surface_capabilities
+            .formats
+            .iter()
+            .copied()
+            .filter(|f| f.is_srgb())
+            .next()
+            .unwrap_or(surface_capabilities.formats[0]);
+
+        let config = wgpu::SurfaceConfiguration{
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.0 as u32,
+            height: size.1 as u32,
+            present_mode: surface_capabilities.present_modes[0],
+            alpha_mode: surface_capabilities.alpha_modes[0],
+            view_formats: vec![],
+            desired_maximum_frame_latency: 2,
+        };
+
+        surface.configure(&device, &config);
+
+        Self {
+            instance,
+            window,
+            surface,
+            device,
+            queue,
+            config,
+            size
+        }
+    }
+}
 
 fn main() {
     let mut glfw = glfw::init(fail_on_errors!()).unwrap();
@@ -19,11 +96,10 @@ fn main() {
                     window.set_should_close(true);
                 }
 
-                _ => {}
-                // uncomment to print events to console
-                //e => { 
-                    //println!("{:?}", e);
-                //}
+                _ => {} // uncomment to print events to console
+                        //e => {
+                        //println!("{:?}", e);
+                        //}
             }
         }
         window.swap_buffers();
